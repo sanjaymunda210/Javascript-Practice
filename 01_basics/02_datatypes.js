8087ce785ed718e7609e726d3dd14cd2132aad0e
SANJAY MUNDA <93436306+sanjaymunda210@users.noreply.github.com>
2024-04-05 06:35:25 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js ode as newer version
+
+/// code readability should be maintained
+
+let name = "sanjay"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
