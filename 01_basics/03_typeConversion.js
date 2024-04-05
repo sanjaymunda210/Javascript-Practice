590f4d5418099274081309f7e0881affcb55a5f9
SANJAY MUNDA <93436306+sanjaymunda210@users.noreply.github.com>
2024-04-05 07:02:30 +0000

    type conversion

---
+++ 01_basics/03_typeConversion.js
@@ -0,0 +1,36 @@
+// let score = "Sanjay"
+
+// two types to access typeof method
+//console.log(typeof score);
+//console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber); 
+// console.log(valueInNumber); 
+
+// type conversion take place but output can be different
+
+// "33" => 33
+// "Sanjay" => NaN
+// null => 0
+// undefined => NaN
+// true => 1
+// false => 0
+
+
+
+// let isLoggedIn = "Sanjay"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Sanjay" => true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
