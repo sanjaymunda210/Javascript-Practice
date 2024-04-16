487152e4f3f16c010e28a8f462c1338dfe5d1a52
Sanjay <sanjaymunda0123@gmail.com>
2024-04-16 12:10:39 +0530

    Conversion Operations

--- 01_basics/03_typeConversion.js
+++ 01_basics/03_typeConversion.js
@@ -5,8 +5,8 @@
 //console.log(typeof(score));
 
 // let valueInNumber = Number(score);
-// console.log(typeof valueInNumber); 
-// console.log(valueInNumber); 
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // type conversion take place but output can be different
 
@@ -17,8 +17,6 @@
 // true => 1
 // false => 0
 
-
-
 // let isLoggedIn = "Sanjay"
 
 // let booleanIsLoggedIn = Boolean(isLoggedIn)
@@ -28,9 +26,54 @@
 // "" => false
 // "Sanjay" => true
 
+// let someNumber = 33
 
-let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+/* ----- Operations ------------ */
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 3);
+// console.log(2 / 3);
+// console.log(2 % 3);
+
+let str1 = "hello";
+let str2 = " sanjay";
+
+let str3 = str1 + str2; // Concat
+// console.log(str3);
+
+// console.log("1" + 2); // 12
+// console.log(1 + "2"); // 12
+// console.log("1" + 2 + 2); // 122
+// console.log(1 + 2 + "2"); // 32
+
+// console.log(((3 + 4) * 5) % 3); // why not use brackets and make code easy to understand and executable
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1, num2, num3;
+
+// num1 = num2 = num3 = 2 + 2;
+// not a good way to assign
+
+// prefix & postfix
+
+let gameCounter = 100;
+console.log(gameCounter);
+gameCounter++;
+console.log(gameCounter);
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
