90535f3dbcd5d60c2fb293bd2094f1956fb96489
Sanjay <sanjaymunda0123@gmail.com>
2024-04-18 10:43:27 +0530

    Stack and Heap

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -32,3 +32,29 @@ const myFunction = function () {
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+// ++++++++++++++++++++++++++++++++++
+
+// two types of memory - stack & heap
+
+// all primitive data type are stored in stack
+// all non-primitive data types are stored in heap
+
+// in stack we get a copy and in heap we get a reference
+
+//example of stack
+let nameOne = "Sanjay";
+let nameTwo = nameOne;
+nameTwo = "Munda";
+console.log(nameOne);
+console.log(nameTwo);
+
+//example of heap
+let userOne = {
+  name: "Sanjay",
+  email: "sanjaymunda0123@gmail.com",
+};
+let userTwo = userOne;
+userTwo.email = "sanjay@google.com";
+console.log(userOne.email);
+console.log(userTwo.email);
